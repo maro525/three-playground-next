7292181dcdf4e9e08c03395f6ba0db69e4dbf1d4
hidemaro <hdmrf.fjnm@gmail.com>
2022-01-04 18:09:06 +0900

    Glitch pass

--- src/components/canvas/Glitch.tsx
+++ src/components/canvas/Glitch.tsx
@@ -1,74 +1,61 @@
-import React, {useEffect, useRef} from "react"
-import { useFrame , useThree } from "@react-three/fiber"
+import React, { useEffect, useRef } from 'react'
+import { useFrame, useThree } from '@react-three/fiber'
 
-import { EffectComposer, RenderPass, GlitchPass } from "three-stdlib"
-import { Object3D, Fog, SphereGeometry, Mesh, MeshPhongMaterial } from "three"
+import { Group, SphereGeometry, Color, Mesh, MeshPhongMaterial } from 'three'
+import { EffectComposer, Glitch } from '@react-three/postprocessing'
+import { GlitchMode } from 'postprocessing'
 
-interface PramasAnimate { object: Object3D
-    composer: EffectComposer
-}
-
-const Glitch: React.FC = () => {
-
-  const { scene, gl, camera, size } = useThree()
+const GlitchView: React.FC = () => {
+  const { scene, camera } = useThree()
 
   const objectRef = useRef(null)
-  const composerRef = useRef(null)
 
-  useEffect(() => init())
+  useEffect(() => init(), [])
   useFrame(() => update())
 
   const init = () => {
+    camera.position.z = 240
 
-          const w = size.width
-          const h = size.height
+    const object = new Group()
+    objectRef.current = object
+    scene.add(object)
 
-          camera.position.z = 240
-
-          gl.setClearColor('#1d1d1d')
-
-          const object = new Object3D()
-          scene.add(object)
-          objectRef.current = object
-
-          const geometry = new SphereGeometry(2, 3, 4)
-          for (let i=0; i<100; i++) {
-            const material = new MeshPhongMaterial({
-              color: 0x000000,
-              flatShading: true
-            })
-            const mesh = new Mesh(geometry, material)
-            mesh.position.set(Math.random()-0.5, Math.random()-0.5, Math.random()-0.1).normalize()
-            mesh.position.multiplyScalar(Math.random() * 400)
-            mesh.rotation.set(Math.random()* 2, Math.random()*2, Math.random()*2)
-            mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 50
-            object.add(mesh)
-          }
-
-          const composer = new EffectComposer(gl)
-          composerRef.current = composer
-          const renderPass = new RenderPass(scene, camera)
-          composer.addPass(renderPass)
-
-          const effectGlitch = new GlitchPass(64)
-          effectGlitch.goWild = false
-          effectGlitch.renderToScreen = true
-          composer.addPass(effectGlitch)
+    const geometry = new SphereGeometry(2, 3, 4)
+    for (let i = 0; i < 100; i++) {
+      const material = new MeshPhongMaterial({
+        color: 0x000000,
+        flatShading: true,
+      })
+      const mesh = new Mesh(geometry, material)
+      mesh.position
+        .set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.1)
+        .normalize()
+      mesh.position.multiplyScalar(Math.random() * 450)
+      mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2)
+      mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 40
+      object.add(mesh)
+    }
   }
 
   const update = () => {
-        objectRef.current.rotation.x += 0.01
-        objectRef.current.rotation.z += 0.01
-        composerRef.current.render()
+    objectRef.current.rotation.x += 0.01
+    objectRef.current.rotation.z += 0.01
   }
 
-  return(
+  return (
     <>
+      {/* light */}
       <directionalLight position={[1, 1, 1]} />
-      <ambientLight color="0x222222" />
-      <fog args={['white', 1, 1000]} />
+      <ambientLight color='#222222' />
+      {/* canvas color */}
+      <color attach='background' args={['#1d1d1d']} />
+      {/* effect */}
+      <fog color={new Color(1)} attach='fog' near={1} far={1000} />
+      <EffectComposer>
+        <Glitch mode={GlitchMode.SPORADIC} />
+      </EffectComposer>
     </>
   )
 }
 
-export default Glitch
+export default GlitchView
